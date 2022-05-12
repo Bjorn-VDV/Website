d7cf0acbc24d558843a2a647993e510a4a529df8
bjorn-vdv <bjorn_vandevelde@hotmail.com>
2022-05-12 14:39:18 +0200

    moved footer to javascript

--- js/scriptstuff.js
+++ js/scriptstuff.js
@@ -23,3 +23,27 @@ window.onclick = function (event)
     }
 }
 
+function footerRepeat()
+{
+    let result = "";
+    // <a href="https://sv-se.facebook.com/" class="positionSticker" target="_blank">
+    //         <img src='../Images/fbLogo.png' alt='Facebook Logo' style='width: 12%; height: 12%;min-width: 25px;'>
+    //         Facebook!</a>
+    //     <a href="https://www.instagram.com/" class="positionSticker" target="_blank">
+    //         <img src="../Images/instaLogo.jpg" alt="Instagram Logo" style="width: 12%; height: 12%;min-width: 25px;">
+    //         Instagram!</a>
+    result += "<div class='dropdown;positionSticker'><button onclick='menuButton()' class='dropbtn'>MENU</button><div id='myDropdown' class='dropdown-content'>" + 
+        "<a href='../index.html'>HOME</a><a href='../html/About.html'>ABOUT</a>" + 
+        "<a href='../html/Products.html'>PRODUCTS</a>" +
+        "<a href='../html/Contact.html'>CONTACT</a>" +     
+        "</div></div>"
+
+    result += "<a href='https://sv-se.facebook.com/' class='positionSticker' target='_blank'>" + 
+    "<img src='../Images/fbLogo.png' alt='Facebook Logo' style='width: 12%; height: 12%;min-width: 25px;'>Facebook!</a>";
+
+    result += "<a href='https://www.instagram.com/' class='positionSticker' target='_blank'>" +
+    "<img src='../Images/instaLogo.jpg' alt='Instagram Logo' style='width: 12%; height: 12%;min-width: 25px;'>Instagram!</a>";
+
+    document.getElementsByTagName("footer")[0].innerHTML = result;
+}
+footerRepeat();
